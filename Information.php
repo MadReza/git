cbb751a16fde2ed7222909280e1b2d7e6b4aabbb
Reza Madabadi <rezmaza@gmail.com>
2020-09-30 15:49:58 -0400

    Added Information.php

---
+++ Information.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Repository: Project, Information
+ *      Need to create a repository
+ *      Do not want a repository inside another repository
+ *
+ * Commands:
+ *      git init [folder location (default to current if left blank)]
+ *          Initialize our repository at the specified location
+ *
+ *      git status
+ *          Tells about the current on goings of our repository
+ *          file changed, added,....
+ *
+ *       git add [file-name]
+ *          Stages the file to be saved(commit).
+ *
+ *       git rm --cached [file-name]
+ *          UnStage the file from being saved(commit).
+ */
